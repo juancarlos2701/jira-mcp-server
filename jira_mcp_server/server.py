30aec720681097970bb257e57d21fac6b2fe93cf
juancarlos2701 <juancarlos.suarez@selleco.com.co>
2025-07-02 16:17:56 +0100

    🚧 Start structure of server.py

--- jira_mcp_server/server.py
+++ jira_mcp_server/server.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from jira-mcp-server!")
+"""
+Jira MCP Server module.
+
+This module provides a FastMCP server for interacting with the Jira API.
+"""
+from dotenv import load_dotenv
+from fastmcp import FastMCP
+
+load_dotenv()
+
+mcp = FastMCP(
+    name="Jira API - MCP Server",
+    instructions="""
+        This server provides tools to interact with the Jira API.
+        You can use the tools to get information about Jira projects, issues, and more.
+        You can also use the tools to create and update issues.
+        You can also use the tools to search for issues.
+        You can also use the tools to get information about Jira users.
+        You can also use the tools to get information about Jira groups.
+    """,
+)
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run()
